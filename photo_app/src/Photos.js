ec9cd6620dcff5c0010edb706685b3ab8df9610c
Jerrysun817 <sunyiran0817@gmail.com>
2023-03-07 01:02:39 -0800

    completed shuffle button and updated comments

--- photo_app/src/Photos.js
+++ photo_app/src/Photos.js
@@ -7,21 +7,49 @@ function Photos() {
     const [photos, setPhotos] = useState([]);
 
     useEffect(() => {
-        const cachedData = localStorage.getItem('photos');  //caching photos to local storage
+        const cachedData = localStorage.getItem('photos');
+        //check if the photos are already cached
         if (cachedData) {
+          //parse the cached data and set state variable to cached photos
           setPhotos(JSON.parse(cachedData));
         } else {
+          //fetch freom API
           fetch('https://jsonplaceholder.typicode.com/photos')
             .then(response => response.json())
             .then(data => {
               setPhotos(data);
+              //store the feched photos to localStorage
               localStorage.setItem('photos', JSON.stringify(data));
             })
             .catch(error => console.log(error));
         }
       }, []);
+    
+    // recursive function to shuffle photos
+    const recurShuffle = (arr) => {
+
+      //base case for 1 photo and no photos
+      if(arr.length === 0 || arr.length === 1) {
+        return arr;
+      }
+      
+      //generate random index within the length of arr
+      const randomIndex = Math.floor(Math.random() * arr.length);
+
+      //remove the photo of randomIndex from arr and add it to tempPhoto
+      const [tempPhoto] = arr.splice(randomIndex, 1);
+
+      //recursive call to the rest of the array
+      return [...recurShuffle(arr), tempPhoto];
+    }
+
+    const shufflePhotos = () => {
+      const shuffledPhotos = recurShuffle([...photos]);
+      setPhotos(shuffledPhotos);
+    };
 
     return (
+      <div>
         <div className='container'>
             {photos.map(photo => (
                 <div key={photo.id}>
@@ -30,6 +58,9 @@ function Photos() {
                 </div>
             ))}
         </div>
+        <button onClick={shufflePhotos}>press to reorder photos</button>
+      </div>
+        
         //change to scroll menu
     );
 }
