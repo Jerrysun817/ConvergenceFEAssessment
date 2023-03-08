d9d4ec0ba547d5f03dc0b0593e06a52390c7902d
Jerrysun817 <sunyiran0817@gmail.com>
2023-03-08 03:44:13 -0800

    implemented carousel and loading screen

--- photo_app/src/Photos.js
+++ photo_app/src/Photos.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import './Photos.css'
-import { CCarouselItem } from '@coreui/react'
+import Loading from "./Loading.js"
 
 function Photos() {
 
   const [photos, setPhotos] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const cachedData = localStorage.getItem('photos');
     //check if the photos are already cached
     if (cachedData) {
       //parse the cached data and set state variable to cached photos
       setPhotos(JSON.parse(cachedData));
+      setLoading(false);
     } else {
       //fetch freom API
       fetch('https://jsonplaceholder.typicode.com/photos')
         .then(response => response.json())
         .then(data => {
           setPhotos(data);
+          setLoading(false);
           //store the feched photos to localStorage
           localStorage.setItem('photos', JSON.stringify(data));
         })
@@ -44,24 +49,42 @@ function Photos() {
   }
 
   const shufflePhotos = () => {
+    setLoading(true);
     const shuffledPhotos = recurShuffle([...photos]);
+    setTimeout(() => { setLoading(false); }, 1000);
     setPhotos(shuffledPhotos);
   };
 
-  return (
+  const nextPhoto = () => {
+    setCurrentIndex(currentIndex === photos.length - 1 ? 0 : currentIndex + 1);
+  }
+
+  const prevPhoto = () => {
+    setCurrentIndex(currentIndex === 0 ? photos.length - 1 : currentIndex - 1);
+  }
+
+  const renderContent = (
     <>
       <div className='container'>
-        {photos.map(photo => (
+        {photos.slice(currentIndex, currentIndex + 5).map(photo => (
           <div key={photo.id} className='photo_container'>
             <img src={photo.url} alt={photo.title} />
             <p>{photo.title}</p>
           </div>
         ))}
       </div>
+      <div className='carousel_buttons'>
+        <button className='prev_button' onClick={prevPhoto}>Prev</button>
+        <button className='next_button' onClick={nextPhoto}>Next</button>
+      </div>
       <button className='shuffle_button' onClick={shufflePhotos}>Press To Reorder Photos</button>
     </>
+  );
 
-    //change to scroll menu
+  return (
+    <>
+      {isLoading ? <Loading /> : renderContent}
+    </>
   );
 }
 
